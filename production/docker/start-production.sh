74ef3b697197df42fd4677c5901c12cfd534647c
Chris Troutner <Chris.troutner@gmail.com>
2022-03-15 22:49:24 +0100

    Fixing typo in docker-compose

--- production/docker/start-production.sh
+++ production/docker/start-production.sh
@@ -5,10 +5,10 @@
 # This mnemonic is used to set up persistent public key for e2ee
 # Replace this with your own 12-word mnemonic.
 # You can get one at https://wallet.fullstack.cash.
-export MNEMONIC="olive two muscle bottom coral ancient wait legend bronze useful process session"
+export MNEMONIC="process cruel lonely arrest ritual bean until abstract craft example process twenty"
 
 # The human readable name this IPFS node identifies as.
-export COORD_NAME=bch-consumer-generic
+export COORD_NAME=pdx01-usa-bch-consumer.fullstackcash.nl
 
 # Allow this node to function as a circuit relay. It must not be behind a firewall.
 #export ENABLE_CIRCUIT_RELAY=true
@@ -17,23 +17,31 @@ export COORD_NAME=bch-consumer-generic
 #export CR_DOMAIN=subdomain.yourdomain.com
 
 # Debug level. 0 = minimal info. 2 = max info.
-export DEBUG_LEVEL=1
+export DEBUG_LEVEL=2
 
 # END: Optional configuration settings
 
 
 # Production database connection string.
-export DBURL=mongodb://172.17.0.1:5555/ipfs-bch-consumer-prod
-
+export DBURL=mongodb://172.17.0.1:5557/ipfs-bch-consumer-prod
 
 # Configure REST API port
-export PORT=5010
+export PORT=5012
 
 # Production settings using external go-ipfs node.
 export CONSUMER_ENV=production
 export IPFS_HOST=172.17.0.1
-export IPFS_API_PORT=5001
-export IPFS_TCP_PORT=4001
+export IPFS_API_PORT=5003
+export IPFS_TCP_PORT=4003
 #export IPFS_WS_PORT=5269
+
+# Decatur
+#export PREFERRED_PROVIDER=12D3KooWBXuJHsAoqkn3NwwFsGzWkNkRYH17RtNJY2snTSxQpikS
+
+# PDX
+export PREFERRED_PROVIDER=12D3KooWSq3FtepuvgqmDjVBjUo2HfH1XQXayQpcRSWvmcjGG28j
+
+# BC Canada
+#export PREFERRED_PROVIDER=12D3KooWHhpe6v5sjiVaYLakt5YqbudRGcZtUo7KskdBotowDu18
 
 npm start
